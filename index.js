af8ae0500033e00a34343f97ed2f57d2f5c6f6c7
Will Hilton <wmhilton@gmail.com>
2017-03-19 23:22:50 -0400

    Rewrite for the better plugin API

--- index.js
+++ index.js
@@ -1,47 +1,35 @@
 'use strict'
 const path = require('path')
-module.exports = function (section) {
+module.exports = function (sections) {
   let pkg = require(path.join(process.cwd(), 'package.json'))
   if (!pkg.name) throw new Error(`package.json is missing a 'name' field.`)
   if (!pkg.description) throw new Error(`package.json is missing a 'description' field.`)
-  // This API might change, but...
-  // for now what I'm doing is saying global modules are handed the 'root' section.
-  // Modules typically start with '# name of package' so there is no body, just subsections.
-  // (However sometimes you will find modules that list their badges at the top, above the module name.)
-  if (!section.subsections[0]) throw new Error(`README must contain a line starting with '#' in order to use the name-and-description plugin.`)
-  let titleSection = section.subsections[0]
-  titleSection.title = ` ${pkg.name}`
-  // Use a heurstic to determine whether to insert or replace the description.
-  if (titleSection.body.length === 0) {
-    // insert is only choice
-    titleSection.body.unshift(pkg.description)
+  let titleSectionString = `# ${pkg.name}\n${pkg.description}\n\n`
+  // Create if missing.
+  if (sections.length === 0) {
+    sections.unshift(titleSectionString)
+    return sections
   }
-  else if (titleSection.body.length === 1 && titleSection.body[0] === '') {
-    // the blank line might be there for a reason
-    titleSection.body.unshift(pkg.description)
+  // Find where the title section is. It should be the first or second.
+  let position = 0
+  // Modules typically start with '# name of package' but sometimes you will find modules
+  // that list their badges at the top, above the module name.
+  if (!sections[0].startsWith('#') && sections.length > 1) position = 1
+  // This is the first header. If it doesn't contain a module name, we
+  // need to insert this section.
+  if (!sections[position].match(/^#\s*@?[a-zA-Z0-9-._/]+$/m)) {
+    sections.splice(position, 0, titleSectionString)
+    return sections
   }
-  else if (titleSection.body.length === 1 && titleSection.body[0] !== '') {
-    // No blank line, (Github default README style!) replace the line
-    titleSection.body[0] = pkg.description
+  // Split apart title section
+  let lines = sections[position].split(/$/gm)
+  // Update the title section (simple case)
+  if (lines.length < 3) {
+    sections[position] = titleSectionString
+    return sections
   }
-  else if (titleSection.body.length > 1 && titleSection.body[0] !== '' && titleSection.body[1] === '') {
-    // A single line of text, followed by a blank line.
-    // That single line of text is definitely the description. Or if not, it will be now.
-    titleSection.body[0] = pkg.description
-  }
-  else if (titleSection.body.length === 2 && titleSection.body[0] === '' && titleSection.body[1] !== '') {
-    // A blank line, followed by a single line of text.
-    // That single line of text is definitely the description. Or if not, it will be now.
-    titleSection.body[1] = pkg.description
-  }
-  else if (titleSection.body.length > 2 && titleSection.body[0] === '' && titleSection.body[1] !== '' && titleSection.body[2] === '') {
-    // A blank line, followed by a single line of text, followed by a blank line.
-    // That single line of text is definitely the description. Or if not, it will be now.
-    titleSection.body[1] = pkg.description
-  }
-  else {
-    // Insert the line. Next time, this line should match one of the above conditions, so it will
-    // be replaced and not duplicated.
-    titleSection.body.unshift(pkg.description)
-  }
+  // Update the title section, being careful about extended descriptions.
+  lines.splice(0, 2, `# ${pkg.name}\n${pkg.description}`)
+  sections[position] = lines.join('')
+  return sections
 }
